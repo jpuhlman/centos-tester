c4bbf1d857150df71af8dcc8c60925ed54b0a6ed
Jeremy Puhlman <jpuhlman@mvista.com>
2019-10-29 13:09:23 -0700

    Add support for EXTRA_REPOS

--- init.sh
+++ init.sh
@@ -12,4 +12,20 @@ baseurl=file:///repo/
 EOF
 fi
 
+if [ -n "$EXTRA_REPOS" ] ; then
+    for repo in $EXTRA_REPOS; do 
+        REPONAME=$(echo "$repo" | cut -d "/" -f 4- | sed s,/,_,g | sed s,_$,,)
+
+cat > /etc/yum.repos.d/$REPONAME.repo << EOF
+[$REPONAME]
+name=$REPONAME
+failovermethod=priority
+enabled=1
+gpgcheck=0
+baseurl=$repo
+EOF
+
+    done
+fi
+
 exec /bin/bash
