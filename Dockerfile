9258890a1efb712842400a45b519e7608776d8fa
Jeremy A. Puhlman <jpuhlman@mvista.com>
2020-09-25 12:38:34 -0700

    6.10

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM centos:7.5.1804
+FROM centos:6.10
 COPY switch-to-vault.sh /
 RUN touch /etc/yum.repos.d/local.repo
 RUN chmod 755 /switch-to-vault.sh; /switch-to-vault.sh; rm -f /switch-to-vault.sh
--- update-info.sh
+++ update-info.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-RELEASE=c7-5-mv
-COMMON=git://gitcentos.mvista.com/centos/upstream/utils/common-build
-REPO=$(basename $COMMON)
-if [ ! -e $(basename $REPO) ] ; then
-    git clone -b $RELEASE $COMMON
-else
-    pushd $REPO
-    git pull
-    popd
-fi
-cat $REPO/conf/centos-updates.cfg | sed /#Base/,/#End/p -n  | sed /^baseurl=/d | sed s,^#baseurl=,baseurl=, > sig.repos
-$REPO/gen-pkgs.sh | sort > pkgs
+RELEASE=c6-10-mv
+COMMON=http://gitcentos.mvista.com/cgit/upstream/utils/common-build.git/plain/
+
+curl -s $COMMON/conf/centos-updates.cfg?h=$RELEASE | sed /#Base/,/#End/p -n  | sed /^baseurl=/d | sed s,^#baseurl=,baseurl=, > sig.repo
+
+curl -s $COMMON/base.list?h=$RELEASE | sort -u | while read REPO A; do
+    if [ "x$A" != "x" ] ; then
+    srpm=$(basename $A)
+    len=$(echo $srpm | sed s,-,\ ,g | wc -w)
+    NAME=$(echo -n $srpm | cut -d "-" -f 1-$(($len - 1)))
+    echo $NAME
+    fi
+done > pkgs
